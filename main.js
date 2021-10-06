44a631dc75e8e204409ca0451b622b1d8205d912
Bankole David <Tomibanks57@gmail.com>
2021-10-06 15:15:58 +0100

    Updated

--- main.js
+++ main.js
@@ -2,29 +2,72 @@
 const addToDoButton = document.getElementById('addToDo');
 const toDoContainer = document.getElementById('toDoContainer');
 const inputField = document.getElementById('inputField');
-const inputField2 = document.getElementById('inputField2');
 const clearToDoButton = document.getElementById('clearToDo');
 
 
 //Event listeners
 addToDoButton.addEventListener('click', addToDo)
 clearToDoButton.addEventListener('click', clearToDo)
+toDoContainer.addEventListener('click', deleteToDo)
+toDoContainer.addEventListener('click', updateToDo)
+
 
 
 //Functons
 function addToDo(e) {
  e.preventDefault();
- const paragraph = document.createElement('p');
- paragraph.classList.add('paragraph-style');
- paragraph.innerText = inputField.value;
- toDoContainer.appendChild(paragraph);
+ const todoDiv = document.createElement('div');
+ todoDiv.classList.add('paragraph-style');
+ todoDiv.innerText = inputField.value;
  inputField.value = "";
+
+ //newTodo
+ //const newTodo = document.createElement('li');
+ //newTodo.innerText = 'hey';
+ //newTodo.classList.add('paragraph-style');
+ //todoDiv.appendChild(newTodo)
+
+ //check mark button
+
+const trashButton = document.createElement('button')
+trashButton.innerHTML = 'delete';
+trashButton.classList.add("complete-btn")
+todoDiv.appendChild(trashButton);
+
+const updateButton = document.createElement('button')
+//updateButton.innerHTML = '<i class="fas fa-trash"></i>';
+updateButton.innerHTML = 'update';
+updateButton.classList.add("complete")
+todoDiv.appendChild(updateButton);
+ 
+
+ //append to toDoContainer
+ toDoContainer.appendChild(todoDiv)
+}
+
+
+function deleteToDo(e) {
+ //console.log(e.target);
+ const item = e.target;
+ if (item.classList[0] === "complete-btn") {
+ const toDoContainer = item.parentElement;
+ toDoContainer.remove();
+ }
+}
+
+function updateToDo(e) {
+ //console.log(e.target);
+ const item = e.target;
+ if (item.classList[0] === "complete") {
+ const toDoContainer = item.parentElement;
+ toDoContainer.remove();
+ }
 }
 
 function clearToDo(e) {
-e.preventDefault();
-const paragraph = document.removeChild('p');
-toDoContainer.replaceChild(paragraph);
-console.log (paragraph)
-
+ console.log(e.target);
+ e.preventDefault();
+ toDoContainer.style.textDecoration = "line-through";
+ //toDoContainer.remove();
+ //toDoContainer.removeChild(todoDiv);
 }
