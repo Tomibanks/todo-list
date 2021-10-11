f6dfeee0ea2352322430ae51e66246a0a409afe3
Bankole David <Tomibanks57@gmail.com>
2021-10-11 23:39:51 +0100

    UPDATE

--- main.js
+++ main.js
@@ -1,69 +1,77 @@
 //selectors
-const addToDoButton = document.getElementById('addToDo');
-const toDoContainer = document.getElementById('toDoContainer');
-const inputField = document.getElementById('inputField');
-const clearToDoButton = document.getElementById('clearToDo');
-
+const addToDoButton = document.getElementById("addToDo");
+const toDoContainer = document.getElementById("toDoContainer");
+const input = document.getElementById("input");
+const clearToDoButton = document.getElementById("clearToDo");
+const submitButton = document.getElementById("submitfield");
 
 //Event listeners
-addToDoButton.addEventListener('click', addToDo)
-clearToDoButton.addEventListener('click', clearToDo)
-toDoContainer.addEventListener('click', deleteToDo)
-//toDoContainer.addEventListener('click', updateToDo)
-
-
+addToDoButton.addEventListener("click", addToDo);
+clearToDoButton.addEventListener("click", clearToDo);
+toDoContainer.addEventListener("click", deleteToDo);
+submitButton.addEventListener("click", submitfield);
 
 //Functons
+
+function save() {
+  var new_data = "" + document.getElementById("input").value;
+
+  if (localStorage.getItem("data") == null) {
+    localStorage.setItem("data", "[]");
+  }
+
+  var old_data = JSON.parse(localStorage.getItem("data"));
+  old_data.push(new_data);
+
+  localStorage.setItem("data", JSON.stringify(old_data));
+}
+
+function view() {
+  if (localStorage.getItem("data") != null) {
+    document.getElementById("output").innerHTML = JSON.parse(
+      localStorage.getItem("data")
+    );
+  }
+}
+
 function addToDo(e) {
- const task = inputField.value;
-if (!task) {
- alert("Please fill out the Todo Task. Love from Bankole David");
- //inputField.value = "Please fill out the todo task";
- return
+  localStorage.setItem("addToDo", addToDoButton);
+  const task = input.value;
+  if (!task) {
+    alert("Please fill out the Todo Task");
+    return;
+  }
+  e.preventDefault();
+  const todoDiv = document.createElement("div");
+  todoDiv.classList.add("paragraph-style");
+  todoDiv.innerText = input.value;
+  input.value = "";
+
+  const trashButton = document.createElement("button");
+  trashButton.innerHTML = '<i class="fas fa-trash"></i>';
+  trashButton.innerHTML = "delete";
+  trashButton.classList.add("delete-btn");
+  todoDiv.appendChild(trashButton);
+
+  //append to toDoContainer
+  toDoContainer.appendChild(todoDiv);
 }
- e.preventDefault();
- const todoDiv = document.createElement('div');
- todoDiv.classList.add('paragraph-style');
- todoDiv.innerText = inputField.value;
- inputField.value = "";
- 
-
-const trashButton = document.createElement('button')
-//trashButton.innerHTML = '<i class="fas fa-trash"></i>';
-trashButton.innerHTML = 'delete';
-trashButton.classList.add("delete-btn")
-todoDiv.appendChild(trashButton);
-
-const updateButton = document.createElement('button')
-updateButton.innerHTML = 'update';
-updateButton.classList.add("update")
-//todoDiv.appendChild(updateButton);
- 
-
- //append to toDoContainer
- toDoContainer.appendChild(todoDiv)
-}
-
 
 function deleteToDo(e) {
- const item = e.target;
- const toDoContainer = item.parentElement;
- toDoContainer.remove();
+  const item = e.target;
+  const toDoContainer = item.parentElement;
+  toDoContainer.remove();
 }
-
-function updateToDo(e) {
- console.log(e.target);
- e.preventDefault();
- 
-}
-
 
 function clearToDo(e) {
- //console.log(e.target);
- e.preventDefault();
- const item = e.target;
- const toDoContainer = item.parentElement;
- //toDoContainer.style.textDecoration = "line-through";
- toDoContainer.remove();
- window.location.reload()
+  //console.log(e.target);
+  e.preventDefault();
+  const item = e.target;
+  const toDoContainer = item.parentElement;
+  toDoContainer.remove();
+  window.location.reload();
+}
+
+function submitfield(e) {
+  window.location.href = "todo.html";
 }
